ecf674c1beec316a1fff45f6dda3f7819026240d
DavidRuff06 <90778233+DavidRuff06@users.noreply.github.com>
2022-08-24 12:56:18 -0500

    onClick is functional but not solving
    Go through the string until space, set that as first number

--- app/src/main/java/com/example/mynewcalc/MainActivity.java
+++ app/src/main/java/com/example/mynewcalc/MainActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    String s = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,14 +15,71 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    public void solve(int i){
 
+    }
+/*
 
-
+ */
     public void onClick(View v) {
         TextView tv = findViewById(R.id.calcScreen);
-        int b = v.getId();
-        String s = "";
-        tv.setText(s + b);
+        int buttonPressed = v.getId();
+        final int ONE = 2131230819;
+        final int TWO = 2131230820;
+        final int THREE =2131230821;
+        final int FOUR = 2131230822;
+        final int FIVE = 2131230823;
+        final int SIX = 2131230824;
+        final int SEVEN = 2131230825;
+        final int EIGHT = 2131230826;
+        final int NINE = 2131230827;
+        final int ZERO = 2131230818;
+        final int CLEAR = 2131230828;
+        final int DIVIDE = 2131230830;
+        final int MULTIPLY = 2131230833;
+        final int MINUS = 2131230832;
+        final int PLUS = 2131230835;
+        final int EQUALS = 2131230831;
+        final int DECIMAL = 2131230829;
+
+
+        if (buttonPressed == ONE){
+            s += "1";
+        } else if(buttonPressed == TWO){
+            s += "2";
+        } else if(buttonPressed == THREE){
+            s += "3";
+        } else if(buttonPressed == FOUR){
+            s += "4";
+        } else if(buttonPressed == FIVE){
+            s += "5";
+        } else if(buttonPressed == SIX){
+            s += "6";
+        } else if(buttonPressed == SEVEN){
+            s += "7";
+        } else if(buttonPressed == EIGHT){
+            s += "8";
+        } else if(buttonPressed == NINE) {
+            s += "9";
+        } else if(buttonPressed == ZERO){
+            s += "0";
+        } else if(buttonPressed == CLEAR){
+            s = "";
+        } else if(buttonPressed == DIVIDE){
+            s += " / ";
+        } else if(buttonPressed == MULTIPLY){
+            s += " * ";
+        } else if(buttonPressed == MINUS){
+            s += " - ";
+        } else if(buttonPressed == PLUS){
+            s += " + ";
+        } else if(buttonPressed == DECIMAL){
+            s += ".";
+        } else if(buttonPressed == EQUALS){
+            s += " = ";
+        }
+
+        tv.setText(s);
 
     }
 
